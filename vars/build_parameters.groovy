25af475aa999fe61cfdadaf871d3b15859d6c50e
khangtictoc <tranhoangkhang09112001@gmail.com>
2025-04-05 12:20:39 +0700

    Add load param meter & confirm run stage 4

--- vars/build_parameters.groovy
+++ vars/build_parameters.groovy
@@ -6,33 +6,59 @@ def call(){
                 choiceType: 'PT_RADIO',
                 name: 'cpu_report',
                 description: 'CPU Report',
-                groovyScript: [
-                    classpath: [],
-                    script: 
-                    """
-                        return ['Enable', 'Disable']
-                    """
+                script: [
+                    $class: 'GroovyScript',
+                    script: [
+                        script:
+                            """
+                            return ['Enable', 'Disable']
+                            """
+                    ]
                 ]
             ],
-            // [
-            //     $class: 'ChoiceParameter',
-            //     choiceType: 'PT_RADIO',
-            //     name: 'memory_report',
-            //     description: 'Memory Report',
-            //     choices: ['Enable', 'Disable']
-            // ],[
-            //     $class: 'ChoiceParameter',
-            //     choiceType: 'PT_RADIO',
-            //     name: 'disk_report',
-            //     description: 'Disk Report',
-            //     choices: ['Enable', 'Disable']
-            // ],[
-            //     $class: 'ChoiceParameter',
-            //     choiceType: 'PT_RADIO',
-            //     name: 'network_report',
-            //     description: 'Network Report',
-            //     choices: ['Enable', 'Disable']
-            // ]
+            [
+                $class: 'ChoiceParameter',
+                choiceType: 'PT_RADIO',
+                name: 'memory_report',
+                description: 'Memory Report',
+                script: [
+                    $class: 'GroovyScript',
+                    script: [
+                        script:
+                            """
+                            return ['Enable', 'Disable']
+                            """
+                    ]
+                ]
+            ],[
+                $class: 'ChoiceParameter',
+                choiceType: 'PT_RADIO',
+                name: 'disk_report',
+                description: 'Disk Report',
+                script: [
+                    $class: 'GroovyScript',
+                    script: [
+                        script:
+                            """
+                            return ['Enable', 'Disable']
+                            """
+                    ]
+                ]
+            ],[
+                $class: 'ChoiceParameter',
+                choiceType: 'PT_RADIO',
+                name: 'network_report',
+                description: 'Network Report',
+                script: [
+                    $class: 'GroovyScript',
+                    script: [
+                        script:
+                            """
+                            return ['Enable', 'Disable']
+                            """
+                    ]
+                ]
+            ]
         ])
     ])
 
